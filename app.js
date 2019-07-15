9d331b7e74ef1359413df87524aab07d1ee833d6
Kevin Auer <kvn35124@uab.edu>
2019-07-15 10:50:55 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 friends = ["Kevin", "Kyle", "Stephen", "Jack", "Bill"];
 let count = 100;
+let btn = document.createElement('button');
+document.body.appendChild(btn);
+let btnText = document.createTextNode('Sing!');
+btn.appendChild(btnText);
 
-for (let i = 0; i < friends.length; i++) {
-    console.log(friends[i]);
-    for (var j = 0; j < 100; j++) {
-        count --
-        count2 = count -1
-        if (count == 1) {
-            var word = "line"
+
+
+btn.addEventListener('click', function () {
+    for (let i = 0; i < friends.length; i++) {
+        console.log(friends[i]);
+        for (var j = 0; j < 100; j++) {
+            count--
+            count2 = count - 1
+            if (count == 1) {
+                var word = "line"
+            }
+            else {
+                var word = "lines"
+            }
+            console.log(count + " " + word + " of code in the file, " + count + " " + word + " of code; " + friends[i] + " strikes one out, clears it all out, " + count2 + " " + word + " of code in the file.");
         }
-        else {
-            var word = "lines"
-        }
-        console.log(count + " " + word + " of code in the file, " + count + " " + word + " of code; " + friends[i] + " strikes one out, clears it all out, " + count2 + " " + word + " of code in the file.");
     }
-}
+})
+
+// for (let i = 0; i < friends.length; i++) {
+//     console.log(friends[i]);
+//     for (var j = 0; j < 100; j++) {
+//         count--
+//         count2 = count - 1
+//         if (count == 1) {
+//             var word = "line"
+//         }
+//         else {
+//             var word = "lines"
+//         }
+//         console.log(count + " " + word + " of code in the file, " + count + " " + word + " of code; " + friends[i] + " strikes one out, clears it all out, " + count2 + " " + word + " of code in the file.");
+//     }
+// }
 
 
